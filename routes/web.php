f5e462664163f16510fea4d5323130693b8ed4c8
muhammadhabibfery <muhammadhabibfery@gmail.com>
2020-09-15 20:53:11 +0700

    add template files

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['prevent-back']], function () {
+    Auth::routes(['register' => false]);
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+});
